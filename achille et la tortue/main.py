e699143dc8535e9ddea8e2e10e402602b70ea714
nolan.mahieu@laplateforme.io <nolan.mahieu@laplateforme.io>
2023-09-06 11:31:06 +0200

    zeno

--- achille et la tortue/main.py
+++ achille et la tortue/main.py
@@ -1,25 +1,34 @@
-#Définition des constantes
+# Définition des constantes
 
+DISTANCE_TOTALE = 100
 VITESSE_ACHILLE = 10
 VITESSE_TORTUE = 1
 
-#Initialisation des positions 
+# Initialisation des positions
 
 position_achille = 0
 position_tortue = 0
 
-#Boucle de simulation 
+# Boucle de simulation
 
-for i in range(100):
-    #Avancement d'Achille
+for i in range(DISTANCE_TOTALE):
+
+    # Avancement d'Achille
+
     position_achille += VITESSE_ACHILLE
 
     # Avancement de la Tortue
+
     position_tortue += VITESSE_TORTUE
 
-    #Affichage des positions
-    print(f"Itération {i}: Achille est à {position_achille} et la Tortue est à {position_tortue}")
+    # Affichage des positions
 
-#Affichage du résultat final
+    print("Achille :", position_achille)
+    print("Tortue :", position_tortue)
 
-print(f"Achille a finalement rattrapé la Tortue à l'itération {i}")
+# Vérification du résultat
+
+if position_achille > position_tortue:
+    print("Achille a gagné !")
+else:
+    print("La Tortue a gagné !")
